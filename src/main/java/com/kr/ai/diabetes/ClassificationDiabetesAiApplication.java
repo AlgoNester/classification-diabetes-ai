08efc2578c663d54546558b22085533fb432f321
Karna <kr.newbee@gmail.com>
2025-09-22 18:33:38 -0700

    Initial commit with Spring AI

---
+++ src/main/java/com/kr/ai/diabetes/ClassificationDiabetesAiApplication.java
@@ -0,0 +1,14 @@
+package com.kr.ai.diabetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassificationDiabetesAiApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(ClassificationDiabetesAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kr/ai/diabetes/ClassificationDiabetesAiApplicationTests.java
@@ -0,0 +1,15 @@
+package com.kr.ai.diabetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassificationDiabetesAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+        // Disable or empty test
+        // If you don't need it, you can even comment out
+	}
+
+}
